5b96e4c127bc3c4512182c51206539b5c2d63480
Enrico Zandomeni Borba <enricozb@gmail.com>
2025-09-28 22:11:06 +0200

    add flake compat

--- flake.nix
+++ flake.nix
@@ -4,7 +4,12 @@
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.fenix.url = "github:nix-community/fenix/monthly";
 
-  outputs = { self, nixpkgs, flake-utils, fenix }:
+  inputs.flake-compat = {
+    url = "github:edolstra/flake-compat";
+    flake = false;
+  };
+
+  outputs = { self, nixpkgs, flake-utils, fenix, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
